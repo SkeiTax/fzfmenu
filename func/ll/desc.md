78ce93469af1223becd7627faf6c271e535bd9ec
SkeiTax <skeitax@dorifeef@gmail.com>
2024-05-22 17:21:04 +0300

    Added ls command

---
+++ func/ll/desc.md
@@ -0,0 +1,4 @@
+# ll
+
+Will run `exa` if it exists,
+otherwise the standard `ls -al` will be executed
---
+++ func/ll/ll.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+if [[ -n `whereis exa | awk '{ print $2 }'` ]]; then
+  exa --icons --color=always --git-ignore -al
+else
+  ls --color -al
+fi
+
