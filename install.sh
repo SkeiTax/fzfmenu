771e3449015573a2f6f5eb81e29721301c0171bf
SkeiTax <skeitax@dorifeef@gmail.com>
2024-05-22 17:20:35 +0300

    Fixed installation

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+echo $SCRIPT_DIR
+
+rm -rf $HOME/.local/bin/fzfmenu.bac
 mv $HOME/.local/bin/fzfmenu $HOME/.local/bin/fzfmenu.bac 2> /dev/null
 rm $HOME/.local/bin/fm 2> /dev/null
 
-cd .. &> /dev/null
-ln -s $SCRIPT_DIR $HOME/.local/bin/fzfmenu &&
+cp -rf $SCRIPT_DIR $HOME/.local/bin/fzfmenu/ &&
 ln -s $HOME/.local/bin/fzfmenu/fmenu.sh $HOME/.local/bin/fm
-cd - &> /dev/null
 
 echo fzfmenu installed!
