5bc98332fbcd46f7b8ec8fef7686ebc2262da442
SkeiTax <skeitax@dorifeef@gmail.com>
2024-05-22 22:11:04 +0300

    Fix for fzfmenu update

--- func/fzfmenu/func/update/update.sh
+++ func/fzfmenu/func/update/update.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 
-git clone https://github.com/SkeiTax/fzfmenu.git --depth 1 $pwd/tmp
-$pwd/tmp/install.sh
-rm -rf $pwd/tmp
+git clone https://github.com/SkeiTax/fzfmenu.git --depth 1 /tmp
+/tmp/install.sh
+rm -rf /tmp
